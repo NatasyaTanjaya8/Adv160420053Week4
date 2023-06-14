cbc29052efd13f8fb2f251a02a21c5a5300925e4
NatasyaTanjaya8 <120316343+NatasyaTanjaya8@users.noreply.github.com>
2023-06-14 13:31:04 +0700

    1

--- app/src/main/java/com/natasya/adv160420053week4/util/util.kt
+++ app/src/main/java/com/natasya/adv160420053week4/util/util.kt
@@ -1,9 +1,17 @@
 package com.natasya.adv160420053week4.util
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.ClipDescription
+import android.content.Context
+import android.os.Build
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.natasya.adv160420053week4.R
+import com.natasya.adv160420053week4.view.MainActivity
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import java.lang.Exception
@@ -18,4 +26,27 @@ fun ImageView.loadImage(url: String?, progressBar: ProgressBar){
 
         }
     })
+}
+fun showNotification(title:String, content:String, icon:Int){
+    val channelId = "${MainActivity.instance?.packageName}-${MainActivity.instance?.getString(R.string.app_name)}"
+    val builder = NotificationCompat.Builder(MainActivity.instance!!.applicationContext, channelId).apply {
+        setSmallIcon(icon)
+        setContentTitle(title)
+        setContentText(content)
+        setStyle(NotificationCompat.BigTextStyle())
+        priority = NotificationCompat.PRIORITY_DEFAULT
+        setAutoCancel(true)
+    }
+    val manager = NotificationManagerCompat.from(MainActivity.instance!!.applicationContext)
+    manager.notify(1001, builder.build())
+}
+fun createNotificationChannel(context: Context, importance:Int, showBadge:Boolean, name:String, description:String){
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        val channelId = "${context.packageName}-$name"
+        val channel = NotificationChannel(channelId, name, importance)
+        channel.description = description
+        channel.setShowBadge(showBadge)
+        val manager = context.getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel)
+    }
 }
--- app/src/main/java/com/natasya/adv160420053week4/view/MainActivity.kt
+++ app/src/main/java/com/natasya/adv160420053week4/view/MainActivity.kt
@@ -2,11 +2,75 @@ package com.natasya.adv160420053week4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+//import androidx.lifecycle.Observer
 import com.natasya.adv160420053week4.R
+import com.natasya.adv160420053week4.util.createNotificationChannel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    init{
+        instance = this
+    }
+    companion object{
+        var instance:MainActivity ?= null
+        fun showNotification(title:String, content:String, icon:Int){
+            val channelId = "${instance?.packageName}-${instance?.getString(R.string.app_name)}"
+            val builder = NotificationCompat.Builder(instance!!.applicationContext, channelId).apply {
+                setSmallIcon(icon)
+                setContentTitle(title)
+                setContentText(content)
+                setStyle(NotificationCompat.BigTextStyle())
+                priority = NotificationCompat.PRIORITY_DEFAULT
+                setAutoCancel(true)
+            }
+            val manager = NotificationManagerCompat.from(instance!!.applicationContext)
+            manager.notify(1001, builder.build())
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createNotificationChannel(this, NotificationManagerCompat.IMPORTANCE_DEFAULT, false, getString(R.string.app_name), "App Student Data")
+        fab.setOnClickListener {
+            val observable = Observable.timer(5, TimeUnit.SECONDS).apply {
+                subscribeOn(Schedulers.io())
+                observeOn(AndroidSchedulers.mainThread())
+                subscribe(){
+                    Log.d("observermessage", "five seconds")
+                    showNotification("Dummy", "A new notification apppeared", R.drawable.ic_baseline_email_24)
+                }
+            }
+        }
+        val observable = Observable.just("Hello", "Welcome to", "Ubaya")
+        val observer = object : Observer<String>{
+            override fun onSubscribe(d: Disposable) {
+                Log.d("Messages", "begin subscribe")
+            }
+
+            override fun onNext(t: String) {
+                Log.d("Messages", "data: $t")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.e("Messages", "error: ${e!!.message.toString()}")
+            }
+
+            override fun onComplete() {
+                Log.d("Messages", "complete")
+            }
+
+        }
+        observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(observer)
     }
 }
